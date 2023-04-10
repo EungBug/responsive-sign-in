97e99368d037f73da3bb52d6baaacba052d78d93
EungBug <eungb14@gmail.com>
2023-04-10 23:50:32 +0900

    Password Visibility Toggle 기능 추가

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 const emailInputEl = document.querySelector('input#email');
 const btnEmailClear = document.querySelector('#email-clear');
 
+const pwInputEl = document.querySelector('input#password');
+const btnPwToggle = document.querySelector('#password-toggle');
+
 btnEmailClear.addEventListener('click', () => {
   emailInputEl.value = '';
+});
+
+btnPwToggle.addEventListener('click', () => {
+  if (pwInputEl.type === 'password') {
+    pwInputEl.type = 'text';
+    btnPwToggle.textContent = 'visibility';
+  } else {
+    pwInputEl.type = 'password';
+    btnPwToggle.textContent = 'visibility_off';
+  }
 });
