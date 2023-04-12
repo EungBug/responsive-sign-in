f390c701da72f3ae7657854fdc12696f59ee3102
EungBug <eungb14@gmail.com>
2023-04-13 01:44:59 +0900

    로그인 버튼 클릭 시 모달 기능 구현

--- main.js
+++ main.js
@@ -17,3 +17,36 @@ btnPwToggle.addEventListener('click', () => {
     btnPwToggle.textContent = 'visibility_off';
   }
 });
+
+// error 모달
+const btnSignIn = document.querySelector('.btn.sign-in');
+const modal = document.querySelector('.modal-wrap');
+const modalMessage = modal.querySelector('h4.message');
+const btnConfirm = modal.querySelector('.btn');
+
+let isShowModal = false;
+
+btnSignIn.addEventListener('click', btnSignInHandler);
+btnConfirm.addEventListener('click', () => {
+  modal.classList.remove('show');
+  isShowModal = false;
+});
+
+function btnSignInHandler() {
+  if (isShowModal) return;
+
+  modalMessage.classList.remove('success');
+
+  if (emailInputEl.value.trim() === 'test@gmail.com' && pwInputEl.value.trim() === '1234') {
+    modalMessage.textContent = '로그인 성공!';
+    modalMessage.classList.add('success');
+  } else if (!emailInputEl.value.trim()) {
+    modalMessage.textContent = '이메일을 입력해주세요!';
+  } else if (!pwInputEl.value.trim()) {
+    modalMessage.textContent = '비밀번호를 입력해주세요!';
+  } else {
+    modalMessage.textContent = '이메일 또는 비밀번호가 올바르지 않습니다.';
+  }
+  modal.classList.add('show');
+  isShowModal = true;
+}
